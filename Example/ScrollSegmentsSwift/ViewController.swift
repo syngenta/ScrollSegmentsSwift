eeeb25a02230a0acb5ef445b3576a613daad8650
Ievgen Iefimenko <raketenok@gmail.com>
2018-08-06 13:28:41 +0300

    Initial commit

---
+++ Example/ScrollSegmentsSwift/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ScrollSegmentsSwift
+//
+//  Created by raketenok@gmail.com on 08/06/2018.
+//  Copyright (c) 2018 raketenok@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
