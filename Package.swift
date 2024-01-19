cc7ed8130ce7a30a5b5d20b7be03d2bf7e35e609
Dmytro Romanov <dimkahr@gmail.com>
2024-01-19 15:24:28 +0100

    COI-60 - Updated UI, added SwiftUI version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "ScrollSegmentsSwift",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
         .library(
